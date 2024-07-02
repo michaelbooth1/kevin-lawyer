c9cfbbaddf709d4c855db94988f52bb770f32b05
michaelbooth1 <michaelbooth08@gmail.com>
2024-07-02 19:25:23 -0400

    init

--- MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c47e7544828d3274badd3784d33fe7018d2715")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d020eefe0595371bce126aaf569e630611e768c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
